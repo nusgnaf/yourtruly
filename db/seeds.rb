0e6f15b348793aa7ad8824c4eff6a8784bf72c77
sfatsdu <nusgnaf@gmail.com>
2022-01-19 11:34:39 +0800

    populate db from valid students csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+File.open('db/data.csv') do |fh|
+  csv = CSV.new(fh, headers: ['name', 'stuid'], converters: [:integer])
+  Student.create(csv.map{ |n| n.to_h})
+end
+
+
+#data = Student.create([{ name: 'fang', stuid: 1111}, {name: 'yuan', stuid: 2222}])
